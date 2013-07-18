20702acdfcb286d107c8aded567d695f0ce5d63b
Craig Blanchette <craig.blanchette@gmail.com>
2013-07-18 09:33:33 -0400

    Removed some old references to getData, using session now so no longer needed.

--- Form/Extension/Spam/EventListener/TimedSpamValidationListener.php
+++ Form/Extension/Spam/EventListener/TimedSpamValidationListener.php
@@ -1,27 +1,27 @@
 <?php
 
-namespace Isometriks\Bundle\SpamBundle\Form\Extension\Spam\EventListener; 
+namespace Isometriks\Bundle\SpamBundle\Form\Extension\Spam\EventListener;
 
-use Symfony\Component\EventDispatcher\EventSubscriberInterface; 
-use Symfony\Component\Form\FormEvents; 
-use Symfony\Component\Form\FormEvent; 
-use Symfony\Component\Form\FormError; 
-use Symfony\Component\Translation\TranslatorInterface; 
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Translation\TranslatorInterface;
 use Isometriks\Bundle\SpamBundle\Form\Extension\Spam\Provider\TimedSpamProviderInterface;
 
 
 class TimedSpamValidationListener implements EventSubscriberInterface
 {
-    private $timeProvider; 
-    private $errorMessage; 
-    private $translator; 
-    private $translationDomain; 
-    private $options; 
-    
-    public function __construct(TimedSpamProviderInterface $timeProvider, 
-                                TranslatorInterface $translator, 
-                                $translationDomain, 
-                                $errorMessage, 
+    private $timeProvider;
+    private $errorMessage;
+    private $translator;
+    private $translationDomain;
+    private $options;
+
+    public function __construct(TimedSpamProviderInterface $timeProvider,
+                                TranslatorInterface $translator,
+                                $translationDomain,
+                                $errorMessage,
                                 $options)
     {
         $this->timeProvider = $timeProvider;
@@ -30,37 +30,34 @@ class TimedSpamValidationListener implements EventSubscriberInterface
         $this->errorMessage = $errorMessage;
         $this->options = $options;
     }
-    
+
     public function preSubmit(FormEvent $event)
     {
         $form = $event->getForm(); 
-        $data = $event->getData(); 
-        
+
         if ($form->isRoot() &&
             $form->getConfig()->getOption('compound') &&
             !$this->timeProvider->isFormTimeValid($form->getName(), $this->options)) {
 
-            $errorMessage = $this->errorMessage; 
+            $errorMessage = $this->errorMessage;
 
             if (null !== $this->translator) {
-                $errorMessage = $this->translator->trans($errorMessage, array(), $this->translationDomain); 
+                $errorMessage = $this->translator->trans($errorMessage, array(), $this->translationDomain);
             }
 
-            $form->addError(new FormError($errorMessage)); 
+            $form->addError(new FormError($errorMessage));
         }
-        
+
         /**
          * Remove the stored time
          */
         $this->timeProvider->removeFormTime($form->getName());
-        
-        $event->setData($data); 
-    }    
-    
+    }
+
     public static function getSubscribedEvents()
     {
         return array(
-            FormEvents::PRE_SUBMIT => 'preSubmit',     
-        ); 
-    }    
+            FormEvents::PRE_SUBMIT => 'preSubmit',
+        );
+    }
 }
